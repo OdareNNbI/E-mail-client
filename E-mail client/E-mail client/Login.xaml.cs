464ae107fced782957aee4596bad017574c91b84
Maxim <31003295+OdareNNbI@users.noreply.github.com>
2018-03-20 00:10:06 +0300

    Application view
    Add Main, Login and SendMessage window and mini-interface for them

---
+++ E-mail client/E-mail client/Login.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+
+namespace E_mail_client
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void signIn_Click(object sender, RoutedEventArgs e)
+        {
+            string gmail = gmailData.Text;
+            string password = passwordData.Password;
+
+            User.SetUser(gmail, password);
+        }
+        
+    }
+}
---
+++ E-mail client/E-mail client/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_mail_client
+{
+    public class User
+    {
+        public static User Account { get; set; }
+
+        public string Gmail { get; private set; }
+        public string Password { get; private set; }
+
+        private User(string Gmail, string Password)
+        {
+            this.Gmail = Gmail;
+            this.Password = Password;
+        }
+
+
+        public static void SetUser(string Gmail, string Password)
+        {
+            if (Account == null)
+                Account = new User(Gmail, Password);
+            else
+            {
+                Account.Gmail = Gmail;
+                Account.Password = Password;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Gmail {Gmail} Password {Password}";
+        }
+    }
+}
